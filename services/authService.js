fcf914b6835331b76f911a4df7d38fced642392c
JuiYuHung-craftcat <juiyuhunghub@gmail.com>
2023-09-06 23:30:39 +0800

    Add login endpoint document and add log message for logging status

--- services/authService.js
+++ services/authService.js
@@ -28,15 +28,17 @@ module.exports = class AuthService {
       // Check if user exists
       const user = await UserModel.findOneByEmail(email);
       if (!user) {
+        console.log("User Not Found");
         throw createError(401, "Incorrect username");
       }
 
       // Check for matching passwords
       const matchFound = await bcrypt.compare(password, user.password);
       if (!matchFound) {
+        console.log("Incorrect Password");
         throw createError(401, "Incorrect password");
       }
-
+      console.log("Loggin Successfully");
       return user;
     } catch (err) {
       throw createError(500, err);
